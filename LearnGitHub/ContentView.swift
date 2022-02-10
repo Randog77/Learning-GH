69cc2529e25d6111b3688522785ab6d28b74cf98
Randall Walker <randog77@gmail.com>
2022-02-10 16:58:45 -0500

    Initial Commit

---
+++ LearnGitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnGitHub
+//
+//  Created by Randall Walker on 2/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnGitHub/LearnGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGitHubApp.swift
+//  LearnGitHub
+//
+//  Created by Randall Walker on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
